ab9bff87e2e03892cb87fcdfb55eb815023703d6
vikalp <vikalpsahni@gmail.com>
2025-03-05 21:23:47 +0530

    first commit

---
+++ ekacare/auth/auth.py
@@ -0,0 +1,56 @@
+from typing import Dict, Any
+
+
+class Auth:
+    """Authentication handler for Eka Care API."""
+    
+    def __init__(self, client):
+        self.client = client
+        
+    def login(self) -> Dict[str, Any]:
+        """
+        Get an access token using client credentials.
+        
+        Returns:
+            dict: Token response containing access_token, refresh_token, etc.
+            
+        Example:
+            >>> client = EkaCareClient(client_id="your_id", client_secret="your_secret")
+            >>> token_response = client.auth.login()
+            >>> print(token_response["access_token"])
+        """
+        response = self.client.request(
+            method="POST",
+            endpoint="/connect-auth/v1/account/login",
+            json={
+                "client_id": self.client.client_id,
+                "client_secret": self.client.client_secret
+            },
+            auth_required=False
+        )
+        return response
+        
+    def refresh_token(self, refresh_token: str) -> Dict[str, Any]:
+        """
+        Refresh an access token using a refresh token.
+        
+        Args:
+            refresh_token (str): The refresh token
+            
+        Returns:
+            dict: Token response containing new access_token, refresh_token, etc.
+            
+        Example:
+            >>> client = EkaCareClient(client_id="your_id", client_secret="your_secret")
+            >>> new_tokens = client.auth.refresh_token("your_refresh_token")
+            >>> client.set_access_token(new_tokens["access_token"])
+        """
+        response = self.client.request(
+            method="POST",
+            endpoint="/connect-auth/v1/account/refresh",
+            json={
+                "refresh_token": refresh_token
+            },
+            auth_required=False
+        )
+        return response
---
+++ integration_test.py
@@ -0,0 +1,22 @@
+from ekacare import EkaCareClient
+from io import BytesIO
+import json
+
+client_id='<Your ID>'
+txn_id = 'test-transaction-id-9'
+extra_data = {'mode':'dictation'}
+action = 'ekascribe'
+json_filepath = f'/Users/Vikalp/Downloads/{txn_id}.json'
+audio_files = [
+    '/Users/Vikalp/Downloads/recording_PT85312001.mp3'
+]
+client = EkaCareClient(
+	client_id=client_id,
+	client_secret='<Your Secret>'
+)
+response = client.files.upload(
+    file_paths=audio_files,
+    txn_id=txn_id,
+    action=action,
+    extra_data=extra_data
+)
