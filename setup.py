14c26322bd436fc974e1e8a736a36b056cf7a004
rakeshateka <rakesh.sabale@ekasupport.com>
2025-06-04 19:34:39 +0530

    version 0.0.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ekacare",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     install_requires=[
         "requests==2.25.0",
